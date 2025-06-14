e2d8e451329d879bfc995dbb15f89e77d4e8af02
jonas pauli <jonaspauli089@gmail.com>
2025-06-15 01:02:17 +0200

    require the last finalized slot from the beacon node to be valid

--- service/src/preprocessor/mod.rs
+++ service/src/preprocessor/mod.rs
@@ -50,7 +50,9 @@ impl Preprocessor {
             "latest_finalized_slot: {}, trusted_slot: {}",
             latest_finalized_slot, self.trusted_slot
         );
-        if ((latest_finalized_slot / 32) * 32) + 1 <= self.trusted_slot {
+        if ((latest_finalized_slot / 32) * 32) + 1 <= self.trusted_slot
+            || latest_finalized_slot % 32 != 0
+        {
             return Err(anyhow::anyhow!(
                 "Waiting for new slot to be finalized, retry in 60 seconds!"
             ));
